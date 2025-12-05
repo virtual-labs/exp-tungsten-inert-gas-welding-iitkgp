43cef5be25ab5881a39d581c8b263eebf159dc5f
vleadadmin <admin@vlabs.ac.in>
2025-12-05 15:37:27 +0000

    https://virtual-labs.github.io/virtual-labs/exp-tungsten-inert-gas-welding-iitkgp click on the link to test your code.

---
+++ simulation/js/adiwe5.js
@@ -0,0 +1,73 @@
+/**
+ This Scrtpt file is developed by
+Aditya Kameswara Rao Nandula
+Senior Project Scientist,
+Virtual Labs IIT Kharagpur.
+LinkedIn: https://in.linkedin.com/in/akraonandula/
+ */
+
+var cmb=0;
+var i=0;
+var i=0;
+$(document).ready(function(){
+    adiwe5(0);
+});
+
+const sizs={
+    wd:window.innerWidth*0.9,
+    ht:window.innerHeight*0.9
+};
+window.addEventListener("resize",()=>{
+    sizs.wd=window.innerWidth*0.9;
+    sizs.ht=window.innerHeight*0.9;
+    window.location.reload();
+    
+
+});
+
+function adiwe5(a){
+    if(a==0){
+        $("#adisim").hide();
+        $('#inst').show();
+        cmb=0;
+        i=0;
+        $("#estr").removeClass('active');
+    }
+    else if(a==1) {
+        cmb=1;
+        $("#adisim").show();
+        $('#inst').hide();
+        $("#estr").removeClass('active');
+        $("#aditig").html("<div> <video id=\"adivid\" preload=\"auto\" autoplay>  <source src=\"./images/DCRP.mp4\" type=\"video/mp4\"> </video></div>");
+    $("#adivid").width('100%');
+    $("#adivid").playbackRate= 1.5;
+    $("#adivid").on("ended",function(){
+     $("#aditig").html("");
+    });
+    }
+    else if(a==2) {
+        cmb=2;
+        $("#adisim").show();
+        $('#inst').hide();
+        $("#estr").removeClass('active');
+        $("#aditig").html("<div> <video id=\"adivid\" preload=\"auto\" autoplay>  <source src=\"./images/DCRP.mp4\" type=\"video/mp4\"> </video></div>");
+    $("#adivid").width('100%');
+    $("#adivid").playbackRate= 1.5;
+    $("#adivid").on("ended",function(){
+     $("#aditig").html("");
+    });
+    }
+    else if(a==3) {
+        cmb=3;
+        $("#adisim").show();
+        $('#inst').hide();
+        $("#estr").removeClass('active');
+        $("#aditig").html("<div> <video id=\"adivid\" preload=\"auto\" autoplay>  <source src=\"./images/ACP.mp4\" type=\"video/mp4\"> </video></div>");
+    $("#adivid").width('100%');
+    $("#adivid").playbackRate= 1.5;
+    $("#adivid").on("ended",function(){
+     $("#aditig").html("");
+    });
+    }
+    
+}
